6e0c6d982952466b3bc1437c323efb7f46e8f257
Rudy <rudyax2004@gmail.com>
2025-07-11 21:14:34 -0600

    primer Commic Tarea

--- principal.py
+++ principal.py
@@ -1,1 +1,15 @@
+class Estudiante:
+    def __init__(self, nombre, carnet, carrera, NotaFinal):
+        self.nombre = nombre
+        self.carnet = carnet
+        self.carrera = carrera
+        self.NotaFinal = NotaFinal
+
+
 print("Actividad 05")
+print("Bienvenido a nuestro programa.")
+print("1.- Registrar un nuevo estudiante")
+print("2.- Mostrar lista de todos los estudiantes registrados")
+print("3.- Buscar un estudiante por su Carné")
+print("4.- Calcular el promedio de notas de todos los estudiantes")
+print("Seleccione la opcion que deseea ")
